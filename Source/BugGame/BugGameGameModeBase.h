31d1c7cfc4d15e13400be7a458e9d99ea93d2750
gcasalis <georgecdep@gmail.com>
2021-11-02 16:11:11 +0000

    Initial commit

---
+++ Source/BugGame/BugGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BugGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUGGAME_API ABugGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
